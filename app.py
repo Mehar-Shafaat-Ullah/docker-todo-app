cb1559412264e142c80b4cdb1b4747a692c40494
Mehar-ShafaMehar-Shafaat-Ullah <meharshafatullah>
2025-05-16 11:47:00 +0000

    Initial commit: Dockerized App with Flask + MySQL

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+todos = []
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_todo():
+    todo = request.json
+    todos.append(todo)
+    return jsonify(todo), 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
